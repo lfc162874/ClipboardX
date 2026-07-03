10b1bbadbbcb84d41b6e0ebd02c073f8d7cfa531
柯南的栖息地 <71418202+lfc162874@users.noreply.github.com>
2026-07-03 11:31:36 +0800

    Add Swift Package manifest

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "ClipboardX",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "ClipboardX", targets: ["ClipboardX"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "ClipboardX",
+            path: "Sources/ClipboardX"
+        )
+    ]
+)
