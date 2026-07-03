2c04fc8d385d188f8fda87a095550cefbff468c1
柯南的栖息地 <71418202+lfc162874@users.noreply.github.com>
2026-07-03 12:08:09 +0800

    Remove duplicate root Swift files

--- Sources/ClipboardX/ClipboardKind.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct ClipboardKind: RawRepresentable, Codable, Equatable {
-    let rawValue: String
-
-    static let text = ClipboardKind(rawValue: "text")
-    static let link = ClipboardKind(rawValue: "link")
-    static let image = ClipboardKind(rawValue: "image")
-}
