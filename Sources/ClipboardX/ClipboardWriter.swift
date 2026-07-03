b5f09f3394bc5b7806837763e6b1322dd2da54f0
柯南的栖息地 <71418202+lfc162874@users.noreply.github.com>
2026-07-03 11:36:20 +0800

    Add clipboard writer

---
+++ Sources/ClipboardX/ClipboardWriter.swift
@@ -0,0 +1,9 @@
+import AppKit
+
+enum ClipboardWriter {
+    static func writeText(_ text: String) {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(text, forType: .string)
+    }
+}
