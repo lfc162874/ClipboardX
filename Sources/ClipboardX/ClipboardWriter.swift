8da84f4f6704c5d4e573ae4b9903168aef38a231
柯南的栖息地 <71418202+lfc162874@users.noreply.github.com>
2026-07-03 12:08:29 +0800

    Remove duplicate root Swift files

--- Sources/ClipboardX/ClipboardWriter.swift
+++
@@ -1,9 +0,0 @@
-import AppKit
-
-enum ClipboardWriter {
-    static func writeText(_ text: String) {
-        let pasteboard = NSPasteboard.general
-        pasteboard.clearContents()
-        pasteboard.setString(text, forType: .string)
-    }
-}
